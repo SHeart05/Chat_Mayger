74d08d203a917b8c8949efdec10724173657c700
crapfruit <oemarcel@gmail.com>
2016-09-02 18:49:45 +0200

    Include landing.contorller
    fix vm.person

--- app/core/landing/landing.controller.js
+++ app/core/landing/landing.controller.js
@@ -1,13 +1,14 @@
 (function () {
     'use strict';
 
-    angular.module('pizzaPalace')
-        .controller('LandingCtrl', LandingCtrl);
+    angular
+      .module('pizzaPalace')
+      .controller('LandingCtrl', LandingCtrl);
 
     function LandingCtrl(){
       var vm = this;
 
-      var vm.person = null;
+      vm.person = null;
     }
 
 })();
